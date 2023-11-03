9d15e7d2fb7fe95479e42ea7fbebfcb874e94633
akshayaca <akshayaca@outlook.com>
2023-11-03 19:16:26 -0400

    Fixed header details

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 /********************************************************************************
 * WEB700 – Assignment 04
 *
-* I declare that this assignment is my own work in accordance with Seneca's
-* Academic Integrity Policy:
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
+* of this assignment has been copied manually or electronically from any other source 
+* (including 3rd party web sites) or distributed to other students.
 *
-* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*Name: Akshayarekha Subhramaniyan Student ID: 166557223 Date: 03-11-2023
 *
-* Name: Akshayarekha Subhramaniyan Student ID: 166557223 Date: 03-11-2023
+* Online (Cyclic) Link: https://combative-dove-fatigues.cyclic.app/
+* 
 *
 ********************************************************************************/
 //Include Dependecies 
